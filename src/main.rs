7482130ac2b4f61a5c1488f2a96501b491a9f5a7
Robert Garrett <robertgarrett404@gmail.com>
2025-10-28 16:23:17 -0500

    Don't have Bevy resize to full-screen
    Now that I have a custom canvas, Bevy should adopt that as it's render
    target rather than further modifying it.

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ fn main() {
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
                 canvas: Some("#boids-canvas".to_owned()),
-                fit_canvas_to_parent: true,
                 prevent_default_event_handling: false,
                 ..default()
             }),
